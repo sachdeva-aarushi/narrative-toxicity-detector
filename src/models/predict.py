042909c0cbf3e12a5069d5ab5f7e436fa794c480
sachdeva-aarushi <aarushisachdeva6@gmail.com>
2026-03-10 21:36:48 +0530

    optimize prediction pipelie

--- src/models/predict.py
+++ src/models/predict.py
@@ -5,16 +5,13 @@ from src.features.feature_engineering import transform_text
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))
 MODEL_PATH = os.path.join(BASE_DIR, "models", "trained_model.pkl")
 VECTORIZER_PATH = os.path.join(BASE_DIR, "models", "vectorizer.pkl")
-def load_artifacts():
-    model = load_object(MODEL_PATH)
-    vectorizer = load_object(VECTORIZER_PATH)
-    return model, vectorizer
+_model = load_object(MODEL_PATH)
+_vectorizer = load_object(VECTORIZER_PATH)
 
 def predict_text(text):
-    model, vectorizer = load_artifacts()
     cleaned_text = clean_text(text)
-    features = transform_text(vectorizer, [cleaned_text])
-    probabilities = model.predict_proba(features)[0]
+    features = transform_text(_vectorizer, [cleaned_text])
+    probabilities = _model.predict_proba(features)[0]
     toxicity_score = float(probabilities[1])
     prediction = "toxic" if toxicity_score >= 0.3 else "neutral"
     toxicity_percentage = round(toxicity_score * 100, 2)
